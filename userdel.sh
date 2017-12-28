0cfcbc7029a4fd2c84ef841ececd16dbdf553929
kerdunet <25631028+kerdunet@users.noreply.github.com>
2017-12-28 11:56:55 +0700

    Create userdel.sh

---
+++ userdel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#del user by aditya [082210988552]
+
+read -p "Isikan username yg akan dihapus: " username
+
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo ""
+	read -p "Apakah Anda benar-benar ingin menghapus akun [$username] [y/n]: " -e -i y REMOVE
+	if [[ "$REMOVE" = 'y' ]]; then
+		passwd -l $username
+		userdel $username
+		echo ""
+		echo "Akun [$username] berhasil dihapus!"
+	else
+		echo ""
+		echo "Penghapusan akun [$username] dibatalkan!"
+	fi
+else
+	echo "Username [$username] belum terdaftar!"
+	exit 1
+fi
