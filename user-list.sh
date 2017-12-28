20c07843d12fa402304cee62ce9dda0e68cd3d34
kerdunet <25631028+kerdunet@users.noreply.github.com>
2017-12-28 12:24:08 +0700

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Adam M - 089639714655
+# 082210988552
+echo ""
+echo "-----------------MENGECEK SEMUA AKUN BY KERDUNET.TOP-----------------"
+echo ""
 echo "-------------------------------"
 echo "USERNAME        TANGGAL EXPIRED"
 echo "-------------------------------"
-while read adam
+while read kerdunet
 do
-        AKUN="$(echo $adam | cut -d: -f1)"
-        ID="$(echo $adam | grep -v nobody | cut -d: -f3)"
+        AKUN="$(echo $kerdunet | cut -d: -f1)"
+        ID="$(echo $kerdunet | grep -v nobody | cut -d: -f3)"
         exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
         if [[ $ID -ge 500 ]]; then
         printf "%-17s %2s\n" "$AKUN" "$exp"
