3cd54be49872c42e55e78535d4bbd19d9ece5c03
kerdunet <25631028+kerdunet@users.noreply.github.com>
2017-12-28 11:28:12 +0700

    kerdunet

--- create-user.sh
+++ create-user.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-#Script auto create user SSH
-#Adam M - 089639714655
+#082210988552
 
-read -p "Username : " Login
+read -p "Username : " kerdunet
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=`curl icanhazip.com`
-useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $kerdunet
+exp="$(chage -l $kerdunet | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $kerdunet &> /dev/null
 echo -e ""
-echo -e "Informasi SSH"
-echo -e "=========-account-=========="
+echo -e "----------------------------"
+echo -e "Data Login:
+echo -e "----------------------------"
 echo -e "Host: $IP"
 echo -e "Port: 22, 143, 443, 109, 110"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "Config OpenVPN: $IP:81/1194-client.ovpn"
-echo -e "-----------------------------"
 echo -e "Aktif Sampai: $exp"
-echo -e "==========================="
+echo -e "----------------------------"
